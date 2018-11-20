fd7b920da97769ddf46dbf0f2af80393c96f2389
IvayloG <inj.iganchev@gmail.com>
2018-11-20 17:02:50 +0200

    chore(IgxTreeGrid): Remove fit. #2921

--- projects/igniteui-angular/src/lib/grids/tree-grid/tree-grid-integration.spec.ts
+++ projects/igniteui-angular/src/lib/grids/tree-grid/tree-grid-integration.spec.ts
@@ -644,7 +644,7 @@ describe('IgxTreeGrid - Integration', () => {
             expect(trans.canUndo).toBe(false);
         }));
 
-        fit('Editing a cell is possible with Hierarchical DS', fakeAsync(() => {
+        it('Editing a cell is possible with Hierarchical DS', fakeAsync(() => {
             fix = TestBed.createComponent(IgxTreeGridRowEditingHierarchicalDSTransactionComponent);
             fix.detectChanges();
             treeGrid = fix.componentInstance.treeGrid as IgxTreeGridComponent;
